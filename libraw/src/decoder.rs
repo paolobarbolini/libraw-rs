3573051db3d17b2e7eef3640821eb08bd1da7782
Brad Alfirevic <brad@genbyte.dev>
2020-11-03 17:05:12 -0600

    Add decode.rs to retrieve raw bayer data

---
+++ libraw/src/decoder.rs
@@ -0,0 +1,36 @@
+use crate::{DecodedImage, Error, Result};
+use libraw_sys as sys;
+
+pub struct Decoder {
+    inner: *mut sys::libraw_data_t,
+}
+
+impl Decoder {
+    pub fn new() -> Self {
+        let inner = unsafe { sys::libraw_init(0) };
+        Self { inner }
+    }
+
+    pub fn decode(self, buf: &[u8]) -> Result<DecodedImage> {
+        Error::check(unsafe {
+            sys::libraw_open_buffer(self.inner, buf.as_ptr() as *const _, buf.len())
+        }).unwrap();
+        Error::check(unsafe { sys::libraw_unpack(self.inner) }).unwrap();
+        debug_assert!(!unsafe{(*self.inner).rawdata.raw_alloc}.is_null());
+
+        let decoded = DecodedImage::new(&unsafe{(*self.inner).rawdata}, &unsafe{(*self.inner).sizes});
+        Ok(decoded)
+    }
+}
+
+impl Drop for Decoder {
+    fn drop(&mut self) {
+        unsafe { sys::libraw_close(self.inner) }
+    }
+}
+
+impl Default for Decoder {
+    fn default() -> Self {
+        Self::new()
+    }
+}
---
+++ libraw/src/sizes.rs
@@ -0,0 +1,35 @@
+use libraw_sys as sys;
+
+pub struct Sizes {
+    pub raw_height: u16,
+	pub raw_width: u16,
+    pub height: u16,
+    pub width: u16,
+	pub top_margin: u16,
+    pub left_margin: u16,
+	pub iheight: u16,
+    pub iwidth: u16,
+	pub raw_pitch: u32,
+	pub pixel_aspect: f64,
+    pub flip: i32
+}
+
+impl Sizes {
+    pub(crate) fn new(sizes: &sys::libraw_image_sizes_t) -> Self {
+        let deref = *sizes;
+
+        Self {
+            raw_height: deref.raw_height,
+            raw_width: deref.raw_width,
+            height: deref.height,
+            width: deref.width,
+            top_margin: deref.top_margin,
+            left_margin: deref.left_margin,
+            iheight: deref.iheight,
+            iwidth: deref.iwidth,
+            raw_pitch: deref.raw_pitch,
+            pixel_aspect: deref.pixel_aspect,
+            flip: deref.flip
+        }
+    }
+}

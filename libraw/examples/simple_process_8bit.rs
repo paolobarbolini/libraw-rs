072f63178165b44eeb7ef21409a847de1a063fb7
ArtyomSinyugin <artyomsinyugin@gmail.com>
2024-04-15 17:32:02 +0300

    adding double gamma settings. By default settings for rec. BT.709 are used: power 2.222(i.e. gamm_0=2.222)  and slope 4.5. For sRGB curve use gamm_0=2.4 and gamm_1=12.92, for linear curve set gamm_0/gamm_1 to 1.0.0

--- libraw/examples/simple_process_8bit.rs
+++ libraw/examples/simple_process_8bit.rs
@@ -6,7 +6,11 @@ use libraw::Processor;
 fn main() {
     let buf = fs::read("raw.RW2").expect("read in");
 
-    let processor = Processor::new();
+    let mut processor = Processor::new();
+
+    processor.gamma(2.4, 12.92); // before starting processing we can set gamma. See double gamma explaination here https://www.libraw.org/docs/API-datastruct-eng.html
+                                                // if gamma values is not set by you processor use defaulf values power 2.222 and slope 4.5 
+                                                
     let processed = processor.process_8bit(&buf).expect("processing successful");
 
     let mut out = File::create("out_8bit.ppm").expect("create out");
